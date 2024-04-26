9dc7bf35a32937cd226b817ec3528e1ba84cb982
Steve Biedermann <hardliner66@gmail.com>
2024-04-26 17:32:30 +0200

    usability

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
-use std::{
-    collections::HashMap,
-    path::{Path, PathBuf},
-};
+use std::{collections::HashMap, path::PathBuf};
 
 use clap::{Parser, ValueEnum};
-use image::{DynamicImage, GenericImageView, Pixel, Pixels, imageops::FilterType};
+use image::{imageops::FilterType, DynamicImage, GenericImageView, Pixel, Pixels};
+use kdtree::distance::squared_euclidean;
+use kdtree::KdTree;
 use ndarray::Array2;
 use serde::{Deserialize, Serialize};
 use twmap::{GameLayer, GameTile, TileFlags, TilemapLayer, TwMap};
 
 #[derive(Parser)]
 struct Args {
-    #[clap(subcommand)]
-    command: Command,
+    #[arg(short, long)]
+    print_default_mappings: bool,
+    #[arg(short, long)]
+    /// The mappings to use for the conversion
+    mappings: Option<PathBuf>,
+    #[arg(short, long, default_value = "1")]
+    /// The size per tile
+    tile_size: u32,
+    /// The filter to use when resizing
+    #[arg(short, long, default_value = "nearest")]
+    resize_filter_type: ResizeFilterType,
+    /// The path to the input PNG file
+    input: Vec<PathBuf>,
 }
 
 #[derive(ValueEnum, Debug, Clone, Copy)]
@@ -32,60 +42,22 @@ pub enum ResizeFilterType {
 impl ResizeFilterType {
     fn into_filter_type(self) -> FilterType {
         match self {
-            ResizeFilterType::Nearest =>FilterType::Nearest,
-            ResizeFilterType::Triangle =>FilterType::Triangle,
-            ResizeFilterType::CatmullRom =>FilterType::CatmullRom,
-            ResizeFilterType::Gaussian =>FilterType::Gaussian,
-            ResizeFilterType::Lanczos3 =>FilterType::Lanczos3,
+            ResizeFilterType::Nearest => FilterType::Nearest,
+            ResizeFilterType::Triangle => FilterType::Triangle,
+            ResizeFilterType::CatmullRom => FilterType::CatmullRom,
+            ResizeFilterType::Gaussian => FilterType::Gaussian,
+            ResizeFilterType::Lanczos3 => FilterType::Lanczos3,
         }
     }
 }
 
-#[derive(Parser)]
-struct GlobalOptions {
-    #[arg(short, long)]
-    /// The mappings to use for the conversion
-    mappings: Option<PathBuf>,
-    #[arg(short, long, default_value = "1")]
-    /// The size per tile
-    tile_size: u32,
-    /// The filter to use when resizing
-    #[arg(short, long, default_value = "nearest")]
-    resize_filter_type: ResizeFilterType,
-}
-
-#[derive(Parser)]
-enum Command {
-    /// Prints the default mappings to stdout
-    ExportMappings,
-    /// Converts a PNG to a tw map
-    Convert {
-        #[clap(flatten)]
-        global: GlobalOptions,
-        /// The path to the input PNG file
-        input_png: PathBuf,
-        #[arg(short, long)]
-        /// The path where the output map will be saved
-        output_map: Option<PathBuf>,
-    },
-    /// Converts all PNGs in a directory to tw maps
-    ConvertDirectory {
-        #[clap(flatten)]
-        global: GlobalOptions,
-        /// Path to the input directory containing PNG files
-        input_directory: PathBuf,
-        /// Path to the output directory where converted maps will be saved
-        #[arg(short, long)]
-        output_directory: Option<PathBuf>,
-    },
-}
-
-pub fn export<S: ::std::hash::BuildHasher>(
+pub fn export(
     path: &PathBuf,
     width: usize,
     height: usize,
     pixels: Pixels<DynamicImage>,
-    mapping: &HashMap<[u8; 4], BlockType, S>,
+    mappings: &[([u8; 4], BlockType)],
+    kdtree: &KdTree<f32, usize, [f32; 4]>,
 ) {
     let mut map = TwMap::parse(include_bytes!("../EMPTY.map")).expect("parsing failed");
     map.load().expect("loading failed");
@@ -104,13 +76,21 @@ pub fn export<S: ::std::hash::BuildHasher>(
     for (x, y, value) in pixels {
         let rgba = value.to_rgba();
         let channels = rgba.channels();
-        game_layer[[y as usize, x as usize]] = GameTile::new(
-            mapping
-                .get(&[channels[0], channels[1], channels[2], channels[3]])
-                .unwrap_or(&BlockType::Empty)
-                .get_id(),
-            TileFlags::empty(),
-        );
+        let nearest_point = kdtree
+            .nearest(
+                &[
+                    channels[0] as f32 / 255.0,
+                    channels[1] as f32 / 255.0,
+                    channels[2] as f32 / 255.0,
+                    channels[3] as f32 / 255.0,
+                ],
+                1,
+                &squared_euclidean,
+            )
+            .unwrap()[0]
+            .1;
+        let point = mappings[*nearest_point];
+        game_layer[[y as usize, x as usize]] = GameTile::new(point.1.get_id(), TileFlags::empty());
     }
 
     // save map
@@ -122,6 +102,7 @@ pub fn export<S: ::std::hash::BuildHasher>(
 pub enum BlockType {
     Empty,
     Hookable,
+    Unhookable,
     Freeze,
     Spawn,
     Start,
@@ -134,6 +115,7 @@ impl BlockType {
         match self {
             BlockType::Empty => 0,
             BlockType::Hookable => 1,
+            BlockType::Unhookable => 2,
             BlockType::Freeze => 9,
             BlockType::Spawn => 192,
             BlockType::Start => 33,
@@ -162,7 +144,13 @@ fn parse_mappings(mappings: Option<PathBuf>) -> Mappings {
     )
 }
 
-fn create_map(mappings: &Mappings, tile_size: u32, resize_filter_type: ResizeFilterType, input_png: &PathBuf, output_map: Option<PathBuf>) {
+fn create_map(
+    mappings: &[([u8; 4], BlockType)],
+    kdtree: &KdTree<f32, usize, [f32; 4]>,
+    tile_size: u32,
+    resize_filter_type: ResizeFilterType,
+    input_png: &PathBuf,
+) {
     // Open the image file
     let mut img = image::open(input_png).expect("Failed to open image");
 
@@ -176,7 +164,7 @@ fn create_map(mappings: &Mappings, tile_size: u32, resize_filter_type: ResizeFil
     }
 
     // Determine output file name based on input or provided name
-    let output_map = output_map.unwrap_or_else(|| input_png.with_extension("map"));
+    let output_map = input_png.with_extension("map");
 
     export(
         &output_map,
@@ -184,50 +172,40 @@ fn create_map(mappings: &Mappings, tile_size: u32, resize_filter_type: ResizeFil
         height as usize,
         img.pixels(),
         mappings,
+        kdtree,
     );
 }
 
 fn main() {
     let args = Args::parse();
-    match args.command {
-        Command::ExportMappings => {
-            println!("{DEFAULT_MAPPING}");
-        }
-        Command::Convert {
-            global,
-            input_png,
-            output_map,
-        } => {
-            let mappings: HashMap<[u8; 4], BlockType> = parse_mappings(global.mappings);
-            create_map(&mappings, global.tile_size, global.resize_filter_type, &input_png, output_map);
-        }
+    if args.print_default_mappings {
+        println!("{DEFAULT_MAPPING}");
+        return;
+    }
+    let mappings: Vec<([u8; 4], BlockType)> = parse_mappings(args.mappings).into_iter().collect();
 
-        Command::ConvertDirectory {
-            global,
-            input_directory,
-            output_directory,
-        } => {
-            let mappings: HashMap<[u8; 4], BlockType> = parse_mappings(global.mappings);
+    let mut kdtree = KdTree::new(4);
+    for (i, ([r, g, b, a], _)) in mappings.iter().enumerate() {
+        kdtree
+            .add(
+                [
+                    *r as f32 / 255.0,
+                    *g as f32 / 255.0,
+                    *b as f32 / 255.0,
+                    *a as f32 / 255.0,
+                ],
+                i,
+            )
+            .unwrap();
+    }
 
-            let output_directory = output_directory.unwrap_or_else(|| input_directory.clone());
-
-            if std::fs::create_dir_all(&output_directory).is_err() {
-                println!("Failed to create output directory.");
-                return;
-            }
-
-            for entry in std::fs::read_dir(input_directory).expect("Failed to read input directory")
-            {
-                let entry = entry.expect("Failed to read entry");
-                let path = entry.path();
-                if path.is_file() && path.extension().map_or(false, |e| e == "png") {
-                    let file_name = path.file_name().unwrap();
-                    let output_map =
-                        output_directory.join(Path::new(file_name).with_extension("map"));
-
-                    create_map(&mappings, global.tile_size, global.resize_filter_type, &path, Some(output_map));
-                }
-            }
-        }
+    for input_png in args.input {
+        create_map(
+            &mappings,
+            &kdtree,
+            args.tile_size,
+            args.resize_filter_type,
+            &input_png,
+        );
     }
 }
